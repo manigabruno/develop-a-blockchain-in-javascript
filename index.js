a4676ed70d7d0c2c9790201c0dfe5298fa0428bc
kadie972 <codeurfou@outlook.com>
2024-01-30 12:50:21 +0100

    explication des variables

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 // creation d'un block
 class Block {
     constructor(prevHash, txs, nonce) {
-        this.prevHash = prevHash;
-        this.txs = txs;
-        this.nonce = nonce;
-        this.time = Date.now();
+        this.prevHash = prevHash; /* hash du bloc précédent */
+        this.txs = txs; /* tableau de transaction */
+        this.nonce = nonce; /* Nonmbre arbitraire ou aléatoire qui sert garantir la confidentialité des communications   */
+        this.time = Date.now(); /* recupération de la date*/
 
     }
     // Définition du hash du bloc précédent
@@ -16,5 +16,20 @@ class Block {
     getMerkleHash() {
         return this.txs.toString()
 
+    }
+}
+class Blockchain {
+    constructor(genesisBlock) {
+        this.blocks = new Array()
+        this.blocks[genesisBlock.getHash()] = genesisBlock;
+    }
+    mineBlock() {
+        // preuve de travail
+        let nonce = Math.floor(Math.random() * 1000) +1;
+        let block = new Block(this.getLastBlock(), ['1', '2'], nonce);
+        this.blocks[block.getHash()] = block;
+    }
+    getLastBlock () {
+        return ''
     }
 }
